29aeaddff21d5677e6b11953deeadc7b5c9aea28
53nigen <korun24276@gmail.com>
2015-11-23 19:41:34 +0900

    clean up test codes

--- RxHttpTests/RxHttpTests.swift
+++ RxHttpTests/RxHttpTests.swift
@@ -10,17 +10,14 @@ class RxHttpClientTest: QuickSpec {
     override func spec() {
         describe("RxHttpClient") {
             it("get") {
-                let disposeBag = DisposeBag()
                 let client = DefaultRxHttpClient(defaultTimeoutSec: 5)
-                client.get(NSURL(string: "http://gochiusa.com")!, parameters: nil, headers: nil)
-                    .observeOn(MainScheduler.sharedInstance)
-                    .subscribeNext({ (data, response) -> Void in
-                        //...do something
-                    })
-                    .addDisposableTo(disposeBag)
                 do {
                     let array = try client.get(NSURL(string: "http://gochiusa.com")!, parameters: nil, headers: nil).toBlocking().first()
-                    NSString(data: array!.0, encoding: NSUTF8StringEncoding)?.containsString("ご注文はうさぎですか")
+                    if let string = NSString(data: array!.0, encoding: NSUTF8StringEncoding) {
+                        string.containsString("ご注文はうさぎですか")
+                    } else {
+                        fail()
+                    }
                 } catch {
                     fail()
                 }
