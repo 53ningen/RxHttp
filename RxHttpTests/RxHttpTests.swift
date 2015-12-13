5dcf341e43ad0f3c437cf334b95895eb68d8db59
53nigen <korun24276@gmail.com>
2015-12-14 00:40:14 +0900

    refinement

--- RxHttpTests/RxHttpTests.swift
+++ RxHttpTests/RxHttpTests.swift
@@ -10,13 +10,10 @@ class RxHttpClientTest: QuickSpec {
     override func spec() {
         describe("RxHttpClient") {
             it("get") {
-                let client = DefaultRxHttpClient(defaultTimeoutSec: 5)
+                let client = DefaultRxHttpClient(defaultTimeoutSec: 0)
                 do {
                     let array = try client.get(NSURL(string: "http://gochiusa.com")!).toBlocking().first()
-                    if let string = NSString(data: array!.0, encoding: NSUTF8StringEncoding)
-                    where string.containsString("ご注文はうさぎですか") {
-                        // success
-                    } else {
+                    if (NSString(data: array!.0, encoding: NSUTF8StringEncoding).map { !$0.containsString("ご注文はうさぎですか") } ?? true) {
                         fail()
                     }
                 } catch {
